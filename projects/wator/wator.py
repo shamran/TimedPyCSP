a041eb2a201cf9b67b86b29cef85bf1793e3d097
Rasmus Ebdrup Sørensen <ebdrup@ebdrup-laptop.(none)>
2009-12-08 08:17:58 +0100

    wator works for a single worldpart. Need to implement exchange of borders for multiple worldparts

--- projects/wator/wator.py
+++ projects/wator/wator.py
@@ -1,10 +1,19 @@
 from pycsp.greenlets import *
 from random import *
 from numpy import *
+import pygame, sys
+from pygame.locals import *
 
 
 EMPTY, FISH, SHARK = range(3)
-TYPE, AGE, MOVED = range(3)
+TYPE, AGE, MOVED, STARVED = range(4)
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+
 @process
 def barrier (nr, cIN, cOUT):
   while True:
@@ -18,11 +27,11 @@ def barrier (nr, cIN, cOUT):
 
 @process
 def start(cOUT):
-  worldsize = 16 #world is symmetric, value*value
+  worldsize = 32 #world is symmetric, value*value
   worldparts = 1 #change to dymamic later
   
   starting_fish = 40
-  starting_sharks = 20
+  starting_sharks = 40
 
   cOUT((starting_fish, starting_sharks, worldsize))
 
@@ -31,21 +40,24 @@ def start(cOUT):
 def worldpart (cIN):
   fish, sharks, worldsize = cIN()
   print "%i fish and %i sharks in the world" %(fish, sharks)
- 
+  windowsurface = pygame.display.set_mode((worldsize, worldsize), 0, 32) 
   #Array is fefined as x,y,type of fish, age, moved 
   #Array is defines as x,y,(FISH or SHARK), (MOVED or NOTMOVED) with a age value at the location
-  mypart = zeros((worldsize,worldsize,3))
+  mypart = zeros((worldsize,worldsize,4))
   #Distribute fish and sharks randomly in the world
 
   assert worldsize**2 > fish+sharks #make sure we have room for fish+sharks
  
-  def getempty(x,y):
+  #FIXME for several worldparts!
+  def getsurroundings(x,y,type):
     empty = []
-    if mypart[x][(y-1)%worldsize][TYPE] == EMPTY: empty.append((x,(y-1)%worldsize)) #Above
-    if mypart[x][(y+1)%worldsize][TYPE] == EMPTY: empty.append((x,(y+1)%worldsize)) #Below
-    if mypart[(x-1)%worldsize][y][TYPE] == EMPTY: empty.append(((x-1)%worldsize,y)) #Left
-    if mypart[(x+1)%worldsize][y][TYPE] == EMPTY: empty.append(((x+1)%worldsize,y)) #Right
+    if mypart[x][(y-1)%worldsize][TYPE] == type: empty.append((x,(y-1)%worldsize)) #Above
+    if mypart[x][(y+1)%worldsize][TYPE] == type: empty.append((x,(y+1)%worldsize)) #Below
+    if mypart[(x-1)%worldsize][y][TYPE] == type: empty.append(((x-1)%worldsize,y)) #Left
+    if mypart[(x+1)%worldsize][y][TYPE] == type: empty.append(((x+1)%worldsize,y)) #Right
     return empty
+
+  
 
   #Populate fish
   i = 0
@@ -65,28 +77,75 @@ def worldpart (cIN):
     y = randint(0,worldsize-1)
     if mypart[x][y][TYPE] == EMPTY:
       mypart[x][y][TYPE] = SHARK
-      mypart[x][y][AGE] = randint(1,3)
+      mypart[x][y][AGE] = randint(0,3)
       mypart[x][y][MOVED] = 0
+      mypart[x][y][STARVED] = 0
+
       i+=1
 
   printworld(mypart)
-  
-  for i in range(worldsize):
-    for j in range(worldsize):
-      f = mypart[j][i]
-      emptyspaces = getempty(j,i)
-      if f[TYPE] == FISH and len(emptyspaces) > 0:
-        spawn = choice(emptyspaces)
-        mypart[spawn[0]][spawn[1]][TYPE] = FISH
-        mypart[spawn[0]][spawn[1]][AGE] = 1
-        mypart[spawn[0]][spawn[1]][MOVED] = 1
-        if f[AGE] < 3: #Delete old fish (so it becomes a move instead of spawn)
-          mypart[spawn[0]][spawn[1]][AGE] = f[AGE] #remember age
-          f[TYPE] = EMPTY
-          f[AGE] = 0
-          f[MOVED] = 0
+  while True: 
+    for i in range(worldsize):
+      for j in range(worldsize):
+        f = mypart[j][i]
+        f[MOVED] = 0
 
-  printworld(mypart)
+    for i in range(worldsize):
+      for j in range(worldsize):
+        f = mypart[j][i]
+        emptyspaces = getsurroundings(j,i, EMPTY)
+        fish = getsurroundings(j,i,FISH)
+        #Fish has not moved or reproduced yet and has room to do so
+        if f[TYPE] == FISH and f[MOVED] == 0 and len(emptyspaces) > 0:
+          spawn = choice(emptyspaces)
+          mypart[spawn[0]][spawn[1]][TYPE] = FISH
+          mypart[spawn[0]][spawn[1]][AGE] = 0
+          mypart[spawn[0]][spawn[1]][MOVED] = 1
+          if f[AGE] < 3: #Delete old fish (so it becomes a move instead of spawn)
+            mypart[spawn[0]][spawn[1]][AGE] = f[AGE]+1 #remember age and increment
+            f[TYPE] = EMPTY
+            f[AGE] = 0
+            f[MOVED] = 0
+          else:
+            f[AGE] = 0 #Age is reset for both new and old fish when a spawn accurs
+        
+        #Sharks want to eat if possible, otherwise move. 
+        if f[TYPE] == SHARK and f[MOVED] == 0: # and (len(emptyspaces) > 0 or (len(fish) > 0)):
+          #Shark is starved and dies
+          if f[STARVED] == 3:
+            f[TYPE] = EMPTY
+            f[AGE] = 0
+            f[MOVED] = 0
+            f[STARVED] = 0
+            continue
+          # Move to fish and eat it
+          try:
+            spawn = choice(fish)
+            mypart[spawn[0]][spawn[1]][STARVED] = 0 #Reset starvation as we eat
+          except IndexError:
+            try:
+              # Move
+              spawn = choice(emptyspaces)
+              mypart[spawn[0]][spawn[1]][STARVED] = f[STARVED]+1
+            except IndexError:
+              #no fish to eat, nowhere to move
+              f[AGE] +=1
+              f[STARVED] +=1
+              continue
+          mypart[spawn[0]][spawn[1]][TYPE] = SHARK
+          mypart[spawn[0]][spawn[1]][AGE] = 0
+          mypart[spawn[0]][spawn[1]][MOVED] = 1
+          if f[AGE] < 10: #Delete old shark (so it becomes a move instead of spawn)
+            mypart[spawn[0]][spawn[1]][AGE] = f[AGE]+1 #remember age and increment
+            f[TYPE] = EMPTY
+            f[AGE] = 0
+            f[MOVED] = 0
+            f[STARVED] = 0
+          else:
+            f[AGE] = 0
+        
+    var = raw_input("continue?")
+    printworld(mypart)
     
 def printworld(part):
   print "World:"
