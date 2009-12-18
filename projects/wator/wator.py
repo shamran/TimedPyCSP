902fbefb10eec46022f9861c754f9314fed4d879
Rasmus Ebdrup Sørensen <ebdrup@ebdrup-laptop.(none)>
2009-12-18 09:24:50 +0100

    border exchange er staqdig ikke helt på plads, og der mangler dynamisk oprettelse af channels mellem worldparts

--- projects/wator/wator.py
+++ projects/wator/wator.py
@@ -1,5 +1,6 @@
 from pycsp.greenlets import *
-from random import *
+import random
+from random import choice as randomchoice 
 from numpy import *
 import pygame, sys
 from pygame.locals import *
@@ -10,14 +11,14 @@ TYPE, AGE, MOVED, STARVED = range(4)
 
 
 @process
-def barrier (nr, cIN, cOUT):
+def barrier (nr, cR, cW):
   while True:
     for i in range(nr):
-       cIN()
-       sleep(1)
+       cR()
+       #sleep(1)
        #print "%i workers in barrier" %(i+1)
     for i in range(nr):
-       cOUT(1)
+       cW(1)
 
 
 @process
@@ -35,11 +36,11 @@ def start(cOUT, nr_partsW):
     cOUT((starting_fish, starting_sharks, world_height, world_width/worldparts,i)) # i is transmitted as a reference to the worldpart
 
 @process
-def worldpart (cIN, cOUT):
+def worldpart (cIN, cOUT, barR, barW, leftR, leftW, rightR, rightW):
   fish, sharks, world_height, part_width, part_id = cIN()
   print "%i fish and %i sharks in the world" %(fish, sharks)
   #Array is fefined as x,y,type of fish, age, moved, starved 
-  mypart = zeros((part_width,world_height,4))
+  mypart = zeros((part_width+2,world_height,4))
 
   assert world_height*part_width > fish+sharks #make sure we have room for fish+sharks
  
@@ -57,42 +58,85 @@ def worldpart (cIN, cOUT):
   #Populate fish
   i = 0
   while i < fish:
-    x = randint(0,part_width-1)
-    y = randint(0,world_height-1)
+    x = random.randint(1,part_width)
+    y = random.randint(0,world_height-1)
     if mypart[x][y][TYPE] == EMPTY:
       mypart[x][y][TYPE] = FISH
-      mypart[x][y][AGE] = randint(1,3)
+      mypart[x][y][AGE] = random.randint(0,3)
       mypart[x][y][MOVED] = 0
       i+=1
 
   #Populate sharks
   i = 0
   while i < sharks:
-    x = randint(0,part_width-1)
-    y = randint(0,world_height-1)
+    x = random.randint(1,part_width)
+    y = random.randint(0,world_height-1)
     if mypart[x][y][TYPE] == EMPTY:
       mypart[x][y][TYPE] = SHARK
-      mypart[x][y][AGE] = randint(0,3)
+      mypart[x][y][AGE] = random.randint(0,3)
       mypart[x][y][MOVED] = 0
       mypart[x][y][STARVED] = 0
 
       i+=1
+  # Temp printing  
+  for i in range(len(mypart[0])):
+    for j in range(len(mypart)):
+      f = mypart[j][i]
+      if f[TYPE] == EMPTY: print ".",
+      if f[TYPE] == FISH: print "/",
+      if f[TYPE] == SHARK: print "|",
+    print""
 
+  #Update shadow rows
+
+  left_col = mypart[1] 
+  right_col = mypart[len(mypart)-2] 
+  cOUT((mypart, part_id))
+
+  @choice
+  def read_left_first(__channel_input):
+    mypart[0] = __channel_input
+    rightW(right_col)
+
+  @choice
+  def write_right_first():
+    mypart[0] = leftR()
+  
+  Alternation([
+    (leftR,"read_left_first(__channel_input)"), 
+    (rightW,right_col,None)]
+    ).execute()
+
+  @choice 
+  def read_right_first(__channel_input):
+    mypart[len(mypart)-1] = __channel_input
+    leftW(left_col)
+
+  @choice
+  def write_left_first():
+    mypart[len(mypart)-1] = rightR()
+
+  Alternation([
+    (rightR,"read_right_first(__channel_input)"), 
+    (leftW,left_col,None)]
+    ).execute()
+
+  
   while True: 
     #reset MOVED before we begin the iteration
     for i in range(world_height):
-      for j in range(part_width):
+      for j in range(1,part_width+1):
         f = mypart[j][i]
         f[MOVED] = 0
 
     for i in range(world_height):
-      for j in range(part_width):
+      for j in range(1,part_width+1):
         f = mypart[j][i]
         emptyspaces = getsurroundings(j,i, EMPTY)
         fish = getsurroundings(j,i,FISH)
         #Fish has not moved or reproduced yet and has room to do so
         if f[TYPE] == FISH and f[MOVED] == 0 and len(emptyspaces) > 0:
-          spawn = choice(emptyspaces)
+          spawn = randomchoice(emptyspaces)
           mypart[spawn[0]][spawn[1]][TYPE] = FISH
           mypart[spawn[0]][spawn[1]][AGE] = 0
           mypart[spawn[0]][spawn[1]][MOVED] = 1
@@ -115,12 +159,12 @@ def worldpart (cIN, cOUT):
             continue
           # Move to fish and eat it
           try:
-            spawn = choice(fish)
+            spawn = randomchoice(fish)
             mypart[spawn[0]][spawn[1]][STARVED] = 0 #Reset starvation as we eat
           except IndexError:
             try:
               # Move
-              spawn = choice(emptyspaces)
+              spawn = randomchoice(emptyspaces)
               mypart[spawn[0]][spawn[1]][STARVED] = f[STARVED]+1
             except IndexError:
               #no fish to eat, nowhere to move
@@ -141,8 +185,9 @@ def worldpart (cIN, cOUT):
         
     #var = raw_input("continue?")
     cOUT((mypart, part_id))
-    
-
+    barW(1)
+    barR()
+  
 
 @process
 def aggregate(partR, nr_partsR, cW):
@@ -180,10 +225,23 @@ ch = Channel()
 start2aggr = Channel()
 part2aggr = Channel()
 aggr2vis = Channel()
+part2bar = Channel()
+bar2part = Channel()
 
+#change to dymanic channel assignment:
+zero2oneL = Channel()
+one2zeroL = Channel()
+zero2oneR = Channel()
+one2zeroR = Channel()
+
+
+
+#def worldpart (cIN, cOUT, barR, barW, leftR, leftW, rightR, rightW):
 Parallel(
   start(ch.writer(), start2aggr.writer()), 
-  2*worldpart(ch.reader(), part2aggr.writer()),
+  worldpart(ch.reader(), part2aggr.writer(), bar2part.reader(), part2bar.writer(),one2zeroL.reader(),zero2oneL.writer(),one2zeroR.reader(),zero2oneR.writer()),
+  worldpart(ch.reader(), part2aggr.writer(), bar2part.reader(), part2bar.writer(),zero2oneR.reader(),one2zeroR.writer(),zero2oneL.reader(),one2zeroL.writer()),
   aggregate(part2aggr.reader(), start2aggr.reader(), aggr2vis.writer()),
-  visualize(aggr2vis.reader())
+  visualize(aggr2vis.reader()),
+  barrier(2, part2bar.reader(), bar2part.writer())
 )
